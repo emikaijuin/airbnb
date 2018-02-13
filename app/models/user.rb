3c7c7a625c30799fffcb272469b54470e0ec4691
emikaijuin <emikaijuin@gmail.com>
2018-02-13 06:34:25 +0000

    Facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  
+  has_many :authentication, dependent: :destroy
+  
+    def self.create_with_auth_and_hash(authentication, auth_hash)
+      user = self.create!(
+          first_name: auth_hash["info"]["first_name"],
+          last_name: auth_hash["info"]["last_name"],
+          email: auth_hash["extra"]["raw_info"]["email"],
+          birthday: auth_hash["user_birthday"]
+          )
+        user.authentications << authentication
+        return user
+    end
+    
+    def fb_token
+        x = self.authentications.find_by(provider: 'facebook')
+        return x.token unless x.nil?
+    end
+    
 end
