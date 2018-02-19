ee988f53ecfca23fb1ef5e11345d1dab24237902
emikaijuin <emikaijuin@gmail.com>
2018-02-19 04:51:44 +0000

    Debugged create method so that listing can successfully save. Private methods were added to only permit the user to submit title and description (to be updated and expanded in later versions.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,6 +4,23 @@ class ListingsController < ApplicationController
         @listings = Listing.all
     end
     
-    def show
+    def new
+        @listing = Listing.new
+        @property_types = PropertyType.all
+        @property_subtypes = PropertySubtype.all
+        @amenities = Amenity.all
+        @safety_amenities = SafetyAmenity.all
+    end
+    
+    def create
+        @listing = Listing.new(listing_params)
+        @listing.save
+        redirect_to listings_path
+    end
+    
+    private
+    
+    def listing_params
+        params.require(:listing).permit(:title, :description)
     end
 end
