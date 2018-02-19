7599225afb220348906bda7bfde7313bd57faea3
emikaijuin <emikaijuin@gmail.com>
2018-02-19 08:15:41 +0000

    Added edit, update, and show methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,14 @@
 class ListingsController < ApplicationController
-    
+    include ListingsHelper
+
     def index
         @listings = Listing.all
     end
     
     def new
         @listing = Listing.new
+        
+        # List all DBs that will be required to show available options for new listing.
         @property_types = PropertyType.all
         @property_subtypes = PropertySubtype.all
         @amenities = Amenity.all
@@ -16,6 +19,21 @@ class ListingsController < ApplicationController
         @listing = Listing.new(listing_params)
         @listing.save
         redirect_to listings_path
+    end
+    
+    def destroy
+    end
+    
+    def edit
+        @listing = current_listing
+    end
+    
+    def update
+        redirect_to listing_path(params[:id])
+    end
+    
+    def show
+        @listing = current_listing
     end
     
     private
