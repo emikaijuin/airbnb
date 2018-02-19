bfa5760783f04f2a4125a59c701ed9dc028f399e
emikaijuin <emikaijuin@gmail.com>
2018-02-19 09:30:13 +0000

    Created method to check if listing is available for a given date range

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,23 @@ class Listing < ActiveRecord::Base
   has_many :keywords, through: :keyword_listings
   
     def is_available?(start_date, end_date)
-        # method to return true or false if listing is available for selected date range
+      # Can only book one year ahead of time
+      max_bookable_date = Date.today >> (12)
+      
+      if start_date < Date.today || end_date > max_bookable_date
+        return false
+      elsif self.dates.length == 0
+        puts "length was zero"
+        return true
+      else
+        range = (start_date..end_date).map(&:to_s)
+        range.each do |date|
+          if self.dates.include?(date)
+            return false
+          end
+        end
+        return true
+      end
     end
     
     def host
