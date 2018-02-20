5fff1d88808cd88390b16dcd856e2cd55cdfb329
emikaijuin <emikaijuin@gmail.com>
2018-02-20 04:44:21 +0000

    Added methods for booking, booking confirmation, and booking success.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -36,9 +36,36 @@ class ListingsController < ApplicationController
         @listing = current_listing
     end
     
+    def book
+        @listing = current_listing
+    end
+    
+    def book_confirmation
+        @user = current_user
+    end
+    
+    def book_check
+        if current_listing.is_available?(params[:start_date],params[:end_date])
+            session[:start_date] = params[:start_date]
+            session[:end_date] = params[:end_date]
+            redirect_to book_confirmation_path
+        else
+          flash[:notice] = "Sorry - some of your dates weren't available."
+          redirect_to book_path
+        end 
+    end
+    
+    def book_success
+        current_user.book! current_listing, time_start: session[:start_date].to_date, time_end: session[:end_date].to_date
+        current_listing.reserve_dates(session[:start_date], session[:end_date])
+        @booking = current_user.bookings.last
+        @booking.confirmation_number = SecureRandom.hex(6)
+        params[:confirmation_number] = @booking.confirmation_number
+    end
+    
     private
     
     def listing_params
-        params.require(:listing).permit(:title, :description)
+        params.require(:listing).permit(:title, :description, :dates)
     end
 end
