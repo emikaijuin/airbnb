4671ac62826adb094a04e607757e439cf4e57c26
emikaijuin <emikaijuin@gmail.com>
2018-02-20 09:35:42 +0000

    Created models for property type and subtype

--- app/models/property_subtype.rb
+++ app/models/property_subtype.rb
@@ -1,2 +1,4 @@
 class PropertySubtype < ActiveRecord::Base
+  has_many :property_subtype_property_types
+  has_many :property_types, through: :property_subtype_property_types
 end
--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -1,2 +1,4 @@
 class PropertyType < ActiveRecord::Base
+  has_many :property_subtype_property_types
+  has_many :property_subtypes, through: :property_subtype_property_types
 end
