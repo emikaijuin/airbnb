2f5bb40e25d03ef491ef340bc93483bd6f0877cf
emikaijuin <emikaijuin@gmail.com>
2018-02-20 04:45:09 +0000

    Fixed method for checking available dates to convert different date formats to the same format (to avoid accidental double bookings). Added method to add reserved dates to booking.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,25 +1,39 @@
 class Listing < ActiveRecord::Base
   
+  acts_as_bookable time_type: :range, bookable_across_occurrences: true
+  
   has_many :keywords, through: :keyword_listings
   
     def is_available?(start_date, end_date)
       # Can only book one year ahead of time
       max_bookable_date = Date.today >> (12)
       
-      if start_date < Date.today || end_date > max_bookable_date
+      if start_date.to_date < Date.today || end_date.to_date > max_bookable_date
         return false
+        # add error message
+      elsif start_date.to_date >= end_date.to_date
+        return false
+        # add error message
       elsif self.dates.length == 0
-        puts "length was zero"
         return true
       else
         range = (start_date..end_date).map(&:to_s)
         range.each do |date|
+          date = date.to_date.strftime # Issue in different formats- converting to date then back to string gets all dates to same format
           if self.dates.include?(date)
             return false
           end
         end
         return true
       end
+    end
+    
+    def reserve_dates(start_date, end_date)
+      range = (start_date..end_date).map(&:to_s)
+      range.each do |date|
+        self.dates << date
+      end
+      self.save
     end
     
     def host
