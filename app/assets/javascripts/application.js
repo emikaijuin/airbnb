121ac4180e3e906f2ef94d74707f472abef9472c
emikaijuin <emikaijuin@gmail.com>
2018-02-21 02:58:31 +0000

    Added Google Maps function to append to div with #id

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: 'roadmap'
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
