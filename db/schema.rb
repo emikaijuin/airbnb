5cd461acd81f4e989971aaf60d504c458d7e8935
emikaijuin <emikaijuin@gmail.com>
2018-02-22 04:28:01 +0000

    Add prices to Listing model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221033402) do
+ActiveRecord::Schema.define(version: 20180222040455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180221033402) do
     t.string  "city"
     t.string  "country"
     t.integer "property_id"
+    t.integer "price"
   end
 
   create_table "properties", force: :cascade do |t|
