d23e065ef84eecd80441dd05e25823befc827eab
emikaijuin <emikaijuin@gmail.com>
2018-02-23 03:24:48 +0000

    Fix formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  def allowed?(action, user)
-    if action == edit
-      return true if user == current_listing.user || user.superadmin
-    else
-      return false
-    end
-  end
-  
+
 end
