0ab73b6f111b819c516380f031bcce88362d49c8
emikaijuin <emikaijuin@gmail.com>
2018-02-27 08:49:06 +0000

    Fix syntax error

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,3 +1,31 @@
 module ListingsHelper
 
+  # def get_listings(params)
+      
+  #   if (params[:check_in] && params[:check_out])
+  #     @listings = []
+  #     Listing.all.each do |listing|
+        
+  #       if listing.is_available?(params[:check_in], params[:check_out])
+  #         @listings << listing
+  #       end
+  #     end
+
+  #   elsif params[:guests]
+  #     @listings = []
+  #     Listing.all.each do |listing|
+  #       if listing.guests >= params[:guests].to_i
+  #         @listings << listing
+  #       end
+  #     end
+      
+      
+  #   else
+  #     @listings = Listing.all
+      
+  #   end
+
+  #   @listings
+  # end
+  
 end
